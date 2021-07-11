592d9381ff024f42e0b9fe82121faa46e284e7e9
Richard Camuccio <rcamuccio@gmail.com>
2021-07-11 11:56:56 -0500

    Split current status function into forecast and time functions

--- zeus.py
+++ zeus.py
@@ -34,120 +34,11 @@ class Zeus:
 		self.__consumer_key = ""
 		self.__consumer_secret = ""
 		self.__url_nws = "https://forecast.weather.gov/MapClick.php?lat=25.9931&lon=-97.5607&FcstType=digitalDWML"
-		self.__url_sunset = "https://api.sunrise-sunset.org/json?lat=25.9931&lng=-97.5607&formatted=0"
+		self.__url_time = "https://api.sunrise-sunset.org/json?lat=25.9931&lng=-97.5607&formatted=0"
 
 	def get_color_palette(self):
 
-
-
 		return
-
-	def get_current_status(self):
-
-		from_zone = tz.tzutc()
-		to_zone = tz.tzlocal()
-
-		print(from_zone)
-		print(to_zone)
-
-		print(" Querying URL:", self.__url_sunset)
-		response_sunset = requests.get(url=self.__url_sunset)
-
-		response_sunset_status_code = response_sunset.status_code
-		response_nws_content_type = response_sunset.headers["content-type"]
-		response_nws_encoding = response_sunset.encoding
-
-		parse_response_sunset = json.loads(response_sunset.text)
-
-		astronomical_twilight_begin_str = parse_response_sunset["results"]["astronomical_twilight_begin"]
-		astronomical_twilight_begin_datetime = datetime.strptime(astronomical_twilight_begin_str, "%Y-%m-%dT%H:%M:%S+00:00")
-		astronomical_twilight_begin_datetime_utc = astronomical_twilight_begin_datetime.replace(tzinfo=from_zone)
-		astronomical_twilight_begin_datetime_local = astronomical_twilight_begin_datetime_utc.astimezone(to_zone)
-
-		nautical_twilight_begin_str = parse_response_sunset["results"]["nautical_twilight_begin"]
-		nautical_twilight_begin_datetime = datetime.strptime(nautical_twilight_begin_str, "%Y-%m-%dT%H:%M:%S+00:00")
-		nautical_twilight_begin_datetime_utc = nautical_twilight_begin_datetime.replace(tzinfo=from_zone)
-		nautical_twilight_begin_datetime_local = nautical_twilight_begin_datetime_utc.astimezone(to_zone)
-
-		civil_twilight_begin_str = parse_response_sunset["results"]["civil_twilight_begin"]
-		civil_twilight_begin_datetime = datetime.strptime(civil_twilight_begin_str, "%Y-%m-%dT%H:%M:%S+00:00")
-		civil_twilight_begin_datetime_utc = civil_twilight_begin_datetime.replace(tzinfo=from_zone)
-		civil_twilight_begin_datetime_local = civil_twilight_begin_datetime_utc.astimezone(to_zone)
-
-		sunrise_str = parse_response_sunset["results"]["sunrise"]
-		sunrise_datetime = datetime.strptime(sunrise_str, "%Y-%m-%dT%H:%M:%S+00:00")
-		sunrise_datetime_utc = sunrise_datetime.replace(tzinfo=from_zone)
-		sunrise_datetime_local = sunrise_datetime_utc.astimezone(to_zone)
-
-		solar_noon_str = parse_response_sunset["results"]["solar_noon"]
-		solar_noon_datetime = datetime.strptime(solar_noon_str, "%Y-%m-%dT%H:%M:%S+00:00")
-		solar_noon_datetime_utc = solar_noon_datetime.replace(tzinfo=from_zone)
-		solar_noon_datetime_local = solar_noon_datetime_utc.astimezone(to_zone)
-
-		sunset_str = parse_response_sunset["results"]["sunset"]
-		sunset_datetime = datetime.strptime(sunset_str, "%Y-%m-%dT%H:%M:%S+00:00")
-		sunset_datetime_utc = sunset_datetime.replace(tzinfo=from_zone)
-		sunset_datetime_local = sunset_datetime_utc.astimezone(to_zone)
-
-		civil_twilight_end_str = parse_response_sunset["results"]["civil_twilight_end"]
-		civil_twilight_end_datetime = datetime.strptime(civil_twilight_end_str, "%Y-%m-%dT%H:%M:%S+00:00")
-		civil_twilight_end_datetime_utc = civil_twilight_end_datetime.replace(tzinfo=from_zone)
-		civil_twilight_end_datetime_local = civil_twilight_end_datetime_utc.astimezone(to_zone)
-
-		nautical_twilight_end_str = parse_response_sunset["results"]["nautical_twilight_end"]
-		nautical_twilight_end_datetime = datetime.strptime(nautical_twilight_end_str, "%Y-%m-%dT%H:%M:%S+00:00")
-		nautical_twilight_end_datetime_utc = nautical_twilight_end_datetime.replace(tzinfo=from_zone)
-		nautical_twilight_end_datetime_local = nautical_twilight_end_datetime_utc.astimezone(to_zone)
-
-		astronomical_twilight_end_str = parse_response_sunset["results"]["astronomical_twilight_end"]
-		astronomical_twilight_end_datetime = datetime.strptime(astronomical_twilight_end_str, "%Y-%m-%dT%H:%M:%S+00:00")
-		astronomical_twilight_end_datetime_utc = astronomical_twilight_end_datetime.replace(tzinfo=from_zone)
-		astronomical_twilight_end_datetime_local = astronomical_twilight_end_datetime_utc.astimezone(to_zone)
-	
-		forecast = self.get_forecast()
-
-		latitude = forecast["latitude"]
-		longitude = forecast["longitude"]
-		area = forecast["area"]
-		height = forecast["height"]
-		time = forecast["time"][0]
-		time_jd = forecast["time_jd"][0]
-		dew_point = forecast["dew_point"][0]
-		heat_index = forecast["heat_index"][0]
-		wind_speed = forecast["wind_speed"][0]
-		cloud_amount = forecast["cloud_amount"][0]
-		prob_of_precip = forecast["prob_of_precip"][0]
-		humidity = forecast["humidity"][0]
-		wind_direction = forecast["wind_direction"][0]
-		temperature = forecast["temperature"][0]
-		gust = forecast["gust"][0]
-		hourly_qpf = forecast["hourly_qpf"][0]
-
-		return {"astronomical_twilight_begin" : astronomical_twilight_begin_datetime_local,
-				"nautical_twilight_begin" : nautical_twilight_begin_datetime_local,
-				"civil_twilight_begin" : civil_twilight_begin_datetime_local,
-				"sunrise" : sunrise_datetime_local,
-				"solar_noon" : solar_noon_datetime_local,
-				"sunset" : sunset_datetime_local,
-				"civil_twilight_end" : civil_twilight_end_datetime_local,
-				"nautical_twilight_end" : nautical_twilight_end_datetime_local,
-				"astronomical_twilight_end" : astronomical_twilight_end_datetime_local,
-				"latitude" : latitude,
-				"longitude" : longitude,
-				"area" : area,
-				"height" : height,
-				"time" : time,
-				"time_jd" : time_jd,
-				"dew_point" : dew_point,
-				"heat_index" : heat_index,
-				"wind_speed" : wind_speed,
-				"cloud_amount" : cloud_amount,
-				"prob_of_precip" : prob_of_precip,
-				"humidity" : humidity,
-				"wind_direction" : wind_direction,
-				"temperature" : temperature,
-				"gust" : gust,
-				"hourly_qpf" : hourly_qpf}
 
 	def get_forecast(self):
 
@@ -300,6 +191,78 @@ class Zeus:
 
 		return
 
+	def get_times(self):
+
+		from_zone = tz.tzutc()
+		to_zone = tz.tzlocal()
+
+		print(from_zone)
+		print(to_zone)
+
+		print(" Querying URL:", self.__url_time)
+		response_sunset = requests.get(url=self.__url_time)
+
+		response_sunset_status_code = response_sunset.status_code
+		response_nws_content_type = response_sunset.headers["content-type"]
+		response_nws_encoding = response_sunset.encoding
+
+		parse_response_sunset = json.loads(response_sunset.text)
+
+		astronomical_twilight_begin_str = parse_response_sunset["results"]["astronomical_twilight_begin"]
+		astronomical_twilight_begin_datetime = datetime.strptime(astronomical_twilight_begin_str, "%Y-%m-%dT%H:%M:%S+00:00")
+		astronomical_twilight_begin_datetime_utc = astronomical_twilight_begin_datetime.replace(tzinfo=from_zone)
+		astronomical_twilight_begin_datetime_local = astronomical_twilight_begin_datetime_utc.astimezone(to_zone)
+
+		nautical_twilight_begin_str = parse_response_sunset["results"]["nautical_twilight_begin"]
+		nautical_twilight_begin_datetime = datetime.strptime(nautical_twilight_begin_str, "%Y-%m-%dT%H:%M:%S+00:00")
+		nautical_twilight_begin_datetime_utc = nautical_twilight_begin_datetime.replace(tzinfo=from_zone)
+		nautical_twilight_begin_datetime_local = nautical_twilight_begin_datetime_utc.astimezone(to_zone)
+
+		civil_twilight_begin_str = parse_response_sunset["results"]["civil_twilight_begin"]
+		civil_twilight_begin_datetime = datetime.strptime(civil_twilight_begin_str, "%Y-%m-%dT%H:%M:%S+00:00")
+		civil_twilight_begin_datetime_utc = civil_twilight_begin_datetime.replace(tzinfo=from_zone)
+		civil_twilight_begin_datetime_local = civil_twilight_begin_datetime_utc.astimezone(to_zone)
+
+		sunrise_str = parse_response_sunset["results"]["sunrise"]
+		sunrise_datetime = datetime.strptime(sunrise_str, "%Y-%m-%dT%H:%M:%S+00:00")
+		sunrise_datetime_utc = sunrise_datetime.replace(tzinfo=from_zone)
+		sunrise_datetime_local = sunrise_datetime_utc.astimezone(to_zone)
+
+		solar_noon_str = parse_response_sunset["results"]["solar_noon"]
+		solar_noon_datetime = datetime.strptime(solar_noon_str, "%Y-%m-%dT%H:%M:%S+00:00")
+		solar_noon_datetime_utc = solar_noon_datetime.replace(tzinfo=from_zone)
+		solar_noon_datetime_local = solar_noon_datetime_utc.astimezone(to_zone)
+
+		sunset_str = parse_response_sunset["results"]["sunset"]
+		sunset_datetime = datetime.strptime(sunset_str, "%Y-%m-%dT%H:%M:%S+00:00")
+		sunset_datetime_utc = sunset_datetime.replace(tzinfo=from_zone)
+		sunset_datetime_local = sunset_datetime_utc.astimezone(to_zone)
+
+		civil_twilight_end_str = parse_response_sunset["results"]["civil_twilight_end"]
+		civil_twilight_end_datetime = datetime.strptime(civil_twilight_end_str, "%Y-%m-%dT%H:%M:%S+00:00")
+		civil_twilight_end_datetime_utc = civil_twilight_end_datetime.replace(tzinfo=from_zone)
+		civil_twilight_end_datetime_local = civil_twilight_end_datetime_utc.astimezone(to_zone)
+
+		nautical_twilight_end_str = parse_response_sunset["results"]["nautical_twilight_end"]
+		nautical_twilight_end_datetime = datetime.strptime(nautical_twilight_end_str, "%Y-%m-%dT%H:%M:%S+00:00")
+		nautical_twilight_end_datetime_utc = nautical_twilight_end_datetime.replace(tzinfo=from_zone)
+		nautical_twilight_end_datetime_local = nautical_twilight_end_datetime_utc.astimezone(to_zone)
+
+		astronomical_twilight_end_str = parse_response_sunset["results"]["astronomical_twilight_end"]
+		astronomical_twilight_end_datetime = datetime.strptime(astronomical_twilight_end_str, "%Y-%m-%dT%H:%M:%S+00:00")
+		astronomical_twilight_end_datetime_utc = astronomical_twilight_end_datetime.replace(tzinfo=from_zone)
+		astronomical_twilight_end_datetime_local = astronomical_twilight_end_datetime_utc.astimezone(to_zone)
+
+		return {"astronomical_twilight_begin" : astronomical_twilight_begin_datetime_local,
+				"nautical_twilight_begin" : nautical_twilight_begin_datetime_local,
+				"civil_twilight_begin" : civil_twilight_begin_datetime_local,
+				"sunrise" : sunrise_datetime_local,
+				"solar_noon" : solar_noon_datetime_local,
+				"sunset" : sunset_datetime_local,
+				"civil_twilight_end" : civil_twilight_end_datetime_local,
+				"nautical_twilight_end" : nautical_twilight_end_datetime_local,
+				"astronomical_twilight_end" : astronomical_twilight_end_datetime_local}
+
 	def send_tweet(self, message="#ctmosays Hello world!"):
 
 		twitter = Twython(self.__consumer_key, self.__consumer_secret, self.__access_token, self.__access_token_secret)
@@ -321,33 +284,36 @@ if __name__ == "__main__":
 
 	res = attr("reset")
 
-	current_status = zeus.get_current_status()
+	times = zeus.get_times()
+
+	astronomical_twilight_begin = times["astronomical_twilight_begin"]
+	nautical_twilight_begin = times["nautical_twilight_begin"]
+	civil_twilight_begin = times["civil_twilight_begin"]
+	sunrise = times["sunrise"]
+	solar_noon = times["solar_noon"]
+	sunset = times["sunset"]
+	civil_twilight_end = times["civil_twilight_end"]
+	nautical_twilight_end = times["nautical_twilight_end"]
+	astronomical_twilight_end = times["astronomical_twilight_end"]
+
+	forecast = zeus.get_forecast()
+
+	latitude = forecast["latitude"]
+	longitude = forecast["longitude"]
+	area = forecast["area"]
+	height = forecast["height"]
+	temperature = forecast["temperature"][0]
+	dew_point = forecast["dew_point"][0]
+	heat_index = forecast["heat_index"][0]
+	wind_speed = forecast["wind_speed"][0]
+	wind_direction = forecast["wind_direction"][0]
+	gust = forecast["gust"][0]
+	humidity = forecast["humidity"][0]
+	cloud_amount = forecast["cloud_amount"][0]
+	prob_of_precip = forecast["prob_of_precip"][0]
+	hourly_qpf = forecast["hourly_qpf"][0]
 
 	print()
-
-	astronomical_twilight_begin = current_status["astronomical_twilight_begin"]
-	nautical_twilight_begin = current_status["nautical_twilight_begin"]
-	civil_twilight_begin = current_status["civil_twilight_begin"]
-	sunrise = current_status["sunrise"]
-	solar_noon = current_status["solar_noon"]
-	sunset = current_status["sunset"]
-	civil_twilight_end = current_status["civil_twilight_end"]
-	nautical_twilight_end = current_status["nautical_twilight_end"]
-	astronomical_twilight_end = current_status["astronomical_twilight_end"]
-	latitude = current_status["latitude"]
-	longitude = current_status["longitude"]
-	area = current_status["area"]
-	height = current_status["height"]
-	temperature = current_status["temperature"]
-	dew_point = current_status["dew_point"]
-	heat_index = current_status["heat_index"]
-	wind_speed = current_status["wind_speed"]
-	wind_direction = current_status["wind_direction"]
-	gust = current_status["gust"]
-	humidity = current_status["humidity"]
-	cloud_amount = current_status["cloud_amount"]
-	prob_of_precip = current_status["prob_of_precip"]
-	hourly_qpf = current_status["hourly_qpf"]
 
 	print("------------------------------------------------------------")
 
@@ -386,10 +352,6 @@ if __name__ == "__main__":
 	print(" Hourly QPF                  ", hourly_qpf, "in", type(hourly_qpf))
 
 	print("------------------------------------------------------------")
-
-	print()
-
-	forecast = zeus.get_forecast()
 
 	print()
 	print(" [Forecast projection]")
